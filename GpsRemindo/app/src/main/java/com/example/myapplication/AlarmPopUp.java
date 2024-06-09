d904cacb2256b3161d2cc9ccd56c173bf8bd55c4
Orçun Atamanoğlu <atamanogluorcun@hotmail.com>
2024-06-09 21:20:29 +0300

    Güncel konum eklendi

--- GpsRemindo/app/src/main/java/com/example/myapplication/AlarmPopUp.java
+++ GpsRemindo/app/src/main/java/com/example/myapplication/AlarmPopUp.java
@@ -1,9 +1,9 @@
 package com.example.myapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.WindowManager;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,25 +11,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class AlarmActivity extends AppCompatActivity {
-    private Button move;
+public class AlarmPopUp extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_alarm);
+        setContentView(R.layout.activity_alarm_pop_up);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        move = findViewById(R.id.btn_menu);
-        move.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(AlarmActivity.this,MainActivity.class);
-                startActivity(intent);
-            }
-        });
+        DisplayMetrics dm =  new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width*.7),(int) (height*.5));
+
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.gravity = Gravity.CENTER;
+        params.x =0;
+        params.y =-20;
+
+        getWindow().setAttributes(params);
     }
 }
--- GpsRemindo/app/src/main/java/com/example/myapplication/MainActivity.java
+++ GpsRemindo/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,25 +1,63 @@
 package com.example.myapplication;
 
+import static androidx.core.location.LocationManagerCompat.getCurrentLocation;
+
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private static final String PREFS_NAME = "NotePrefs";
+    private static final String KEY_NOTE_COUNT = "NoteCount";
+    private LinearLayout notesContainer;
+    private List<Note> noteList;
+    private Marker currentMarker;
     private GoogleMap myMap;
+    private MarkerOptions markerOptions;
+    Location currentLocation;
+    private final int FINE_PERMISSION_CODE = 1;
+    SupportMapFragment mapFragment;
+    FusedLocationProviderClient fusedLocationProviderClient;
     private Button move;
     private Button alarm;
 
@@ -33,12 +71,13 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        notesContainer = findViewById(R.id.notesContainer);
 
         move = findViewById(R.id.btn_menu);
         move.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,MenuActivity.class);
+                Intent intent = new Intent(MainActivity.this, MenuActivity.class);
                 startActivity(intent);
             }
         });
@@ -46,20 +85,71 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         alarm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,AlarmActivity.class);
+                Intent intent = new Intent(MainActivity.this, AlarmActivity.class);
                 startActivity(intent);
             }
         });
 
+        fusedLocationProviderClient = (FusedLocationProviderClient) LocationServices.getFusedLocationProviderClient(this);
+        getLastLocation();
+    }
+    private void getLastLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},FINE_PERMISSION_CODE);
+            return;
+        }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if(location != null){
+                    currentLocation = location;
 
+                    mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+                    assert mapFragment != null;
+                    mapFragment.getMapAsync(MainActivity.this);
+                }
+            }
+        });
     }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
-        myMap = googleMap;
+    myMap = googleMap;
+    LatLng sydney = new LatLng(currentLocation.getLatitude(),currentLocation.getLongitude());
+    myMap.addMarker(new MarkerOptions().position(sydney).title("me"));
+    myMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
-        LatLng sydney = new LatLng(-34, 151);
-        myMap.addMarker(new MarkerOptions().position(sydney).title("Sydney"));
-        myMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                // Eğer mevcut bir marker varsa, onu sil
+                if (currentMarker != null) {
+                    currentMarker.remove();
+                }
+                // Yeni marker'ı ekler
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(latLng);
+                markerOptions.title("Buraya tıkladınız!");
+                currentMarker = googleMap.addMarker(markerOptions);
+                CircleOptions circleOptions = new CircleOptions();
+                circleOptions.center(latLng);
+                circleOptions.radius(50); // Yarıçapı metre cinsinden belirtin
+                circleOptions.strokeWidth(0f); // Çizgi kalınlığı
+                circleOptions.fillColor(0x550000FF); // Dairenin iç rengi ve saydamlığı
+                googleMap.addCircle(circleOptions);
+            }
+        });
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == FINE_PERMISSION_CODE){
+            if(grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getLastLocation();
+            }else{
+                Toast.makeText(this,"konum izni veriniz",Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
